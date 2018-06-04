92572fe13a0a88dd256677737008bda4bdb6d11e
Qinyi Cai <CaiQinyi@foxmail.com>
2018-06-04 22:56:09 +0800

    Update Helper.m

--- Helper.m
+++ Helper.m
@@ -1,12 +1,10 @@
 function [X,Q]=Helper(A,tau,l,p,Q)
 for i=1:2
-    [W,R]=house(A*A'*Q);
-    Q=formQ(W);
+    [Q,R]=qr(A*A'*Q);
     Q=Q(:,1:size(R,2));
     R=R(1:size(R,2),:);
 end
-[W,C]=house(A'*Q);
-H=formQ(W);
+[H,C]=qr(A'*Q);
 H=H(:,1:size(C,2));
 C=C(1:size(C,2),:);
 [W,P]=Polar(C);
